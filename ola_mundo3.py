83c898a53cfdbd48f009305033f6bc5ad0fca77f
Hydr0xygenZ <davidrodrigues9@hotmail.com>
2024-07-02 09:32:04 +0100

    First

---
+++ ola_mundo3.py
@@ -0,0 +1,7 @@
+prim_nome = input ("Qual o primeiro nome: ")
+seg_nome = input ("Qual o segundo nome: ")
+prim_nome2 = input ("Qual o outro primeiro nome: ")
+seg_nome2 = input ("Qual o outro segundo nome: ")
+
+print("Olá, ", prim_nome, seg_nome)
+print("e olá, ", prim_nome2, seg_nome2)
